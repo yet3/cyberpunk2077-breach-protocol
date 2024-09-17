3ae7728a6c91f25f783b9841477cc5fe1431d2db
Max Kasperowicz <yet3.dev@gmail.com>
2024-09-17 19:22:58 +0200

    move non-li elements outside ul

--- src/modules/Sequences/Sequences.tsx
+++ src/modules/Sequences/Sequences.tsx
@@ -18,18 +18,20 @@ export const Sequences = () => {
       <div className="animate-scale-from-left flex flex-col relative">
         <div className="drop-shadow-primary-500">
           <SequencesHeader />
-          <ul
-            className="anim__scale-form-top grid border border-primary-500/70 border-t-0 bg-page relative"
+          <div
+            className="anim__scale-form-top relative border border-primary-500/70 border-t-0 bg-page"
             data-2ed-delay="true"
             style={{
               padding: "var(--sequences-inner-py) var(--sequences-inner-px)",
             }}
           >
             <SequencesColHighlight />
-            {sequences.map((sequence) => (
-              <Sequence key={sequence.id} sequence={sequence} />
-            ))}
-          </ul>
+            <ul className="grid">
+              {sequences.map((sequence) => (
+                <Sequence key={sequence.id} sequence={sequence} />
+              ))}
+            </ul>
+          </div>
         </div>
 
         <SequencesBottomDecorations />
